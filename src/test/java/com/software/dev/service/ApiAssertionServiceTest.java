7d2a97a3ec4517918368093e743384067c7c71a8
Moshow郑锴 <moshowgame@126.com>
2025-11-24 21:09:40 +0800

    fix test case issue

--- src/test/java/com/software/dev/service/ApiAssertionServiceTest.java
+++ src/test/java/com/software/dev/service/ApiAssertionServiceTest.java
@@ -2,15 +2,13 @@ package com.software.dev.service;
 
 import com.software.dev.entity.ApiAssertion;
 import com.software.dev.mapper.ApiAssertionMapper;
+import com.software.dev.service.impl.ApiAssertionServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
 import java.util.Arrays;
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
